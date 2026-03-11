bc025980925213fe8d6e93aeb9d8caad1de87e72
B1Fr0st <lovewolves413@gmail.com>
2026-03-11 10:02:21 -0700

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+#![windows_subsystem = "windows"] // hide console window on Windows in release
+#![allow(dead_code)]
+
+#[cfg(not(target_os = "windows"))]
+compile_error!("This application only supports Windows OS!");
+
+pub mod config_system;
+pub use config_system::*;
+
+mod aimbot;
+mod esp;
+mod models;
+mod offsets;
+mod player;
+
+use crate::player::AppData;
+
+const SCHEMA_TOML: &str = include_str!("../config_schema.toml");
+
+fn main() {
+    newbase::init::custom_builder(AppData::default(), "deadlock.exe", Some(1422450))
+        .expect("Failed to initialize runtime")
+        .with_logic(esp::system())
+        .with_config_schema_str(SCHEMA_TOML, "config.toml")
+        .with_thread("players", esp::cache_players)
+        .with_thread("aimbot", aimbot::run)
+        .run();
+}
